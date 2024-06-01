36cab24fe51c347834aed1a8bb6f5f854a9edee8
a1trope <bigtinyturtle@gmail.com>
2024-06-01 15:16:16 +0400

    update views

--- riotrecords/catalog/urls.py
+++ riotrecords/catalog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "catalog"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:item_id>/", views.detail, name="detail")
 ]
--- riotrecords/catalog/views.py
+++ riotrecords/catalog/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
+
 
 def index(request):
-    return HttpResponse("Here will be all items")
+    return render(request, "catalog/index.html", context={"Items": Item.objects.all()})
+
+def detail(request, item_id):
+    # return render(request, "catalog/detail.html", context={"Items": Item.objects.all()})
+    return HttpResponse(f"You are looking at item {item_id}")
