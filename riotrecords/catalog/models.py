62a18ec16289754a66b1f53979147bd264b2eec7
a1trope <bigtinyturtle@gmail.com>
2024-06-10 23:12:48 +0400

    create new models

--- riotrecords/catalog/models.py
+++ riotrecords/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Item(models.Model):
@@ -11,3 +12,22 @@ class Item(models.Model):
     def __str__(self):
         return f"{self.album_name} - {self.band_name}"
 
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=30, choices=[
+        ("PD", "pending"),
+        ("PR", "processing"),
+        ("SH", "shipped"),
+        ("DE", "delivered"),
+    ])
+    address = models.CharField(max_length=200)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return f"order {self.id} (user={self.user})"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="+")
