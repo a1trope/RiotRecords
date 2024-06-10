8b469b755657cf986ae2b15dcd3bcba2d30c9bb4
a1trope <bigtinyturtle@gmail.com>
2024-06-10 23:13:04 +0400

    link new models to admin panel

--- riotrecords/catalog/admin.py
+++ riotrecords/catalog/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Order, OrderItem
 
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ["album_name", "band_name", "price", "year"]
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "status", "address", "time"]
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order_id", "item_id"]
+
+
 admin.site.register(Item, ItemAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
