edba7c6bdd0ba6ec686489930022e47bb80cb7c8
a1trope <bigtinyturtle@gmail.com>
2024-06-11 00:02:34 +0400

    update

---
+++ riotrecords/stats/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+app_name = "stats"
+urlpatterns = [
+    path("", views.get_total_sales, name="get_total_sales"),
+    path("<int:item_id>/", views.get_item_sales, name="get_item_sales"),
+]
--- riotrecords/stats/views.py
+++ riotrecords/stats/views.py
@@ -2,11 +2,13 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 
-def get_total_sales(request, time):
+def get_total_sales(request):
+    # TODO
     # Return total sales for the last month
     return HttpResponse()
 
 
 def get_item_sales(request, item_id):
+    # TODO
     # Return sales of current item
     return HttpResponse()
