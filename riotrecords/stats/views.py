7a8bc1cd1786c89d3910d468c7aa7d1c451cc7ad
a1trope <bigtinyturtle@gmail.com>
2024-06-10 23:59:36 +0400

    added blank views

--- riotrecords/stats/views.py
+++ riotrecords/stats/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_total_sales(request, time):
+    # Return total sales for the last month
+    return HttpResponse()
+
+
+def get_item_sales(request, item_id):
+    # Return sales of current item
+    return HttpResponse()
