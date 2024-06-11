e6438a33c060ce9170bf144c08e5f5f65793e8e7
a1trope <bigtinyturtle@gmail.com>
2024-06-11 19:41:22 +0400

    work in progress

--- riotrecords/stats/views.py
+++ riotrecords/stats/views.py
@@ -1,12 +1,38 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.db.models.functions import TruncDay, TruncMonth
+from django.db.models import Count
+import catalog.models
+
+# print("\n\n----------- IN STATS VIEW TEST -----------------")
+#
+# # Get all sales (delivered orders) from database
+# orders = (catalog.models.Order.objects
+#           .filter(status="DE")
+#           .annotate(day_time=TruncDay("time"))
+#           .values("day_time")
+#           .annotate(dcount=Count("day_time"))
+#               )
+#
+# for order in orders:
+#     print(order)
+#
+# # print(orders)
+#
+# print("------------------------------------------------\n\n")
 
 
 def get_total_sales(request):
     labels = []
     data = []
 
-    # TODO: Get all sales from database
+    # В данной хранится пары <дата с точностью до дня, сколько заказов выполнено в этот день>
+    orders = (catalog.models.Order.objects
+              .filter(status="DE")
+              .annotate(day_time=TruncDay("time"))
+              .values("day_time")
+              .annotate(dcount=Count("day_time"))
+              )
 
     return render(request, "stats/charts.html", context={
         "labels": labels,
