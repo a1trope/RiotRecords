60d554184dd3f829b91d34301b0d4ae4a696154b
a1trope <bigtinyturtle@gmail.com>
2024-06-14 19:47:16 +0400

    Getting sales charts for specified item

--- riotrecords/stats/views.py
+++ riotrecords/stats/views.py
@@ -4,25 +4,29 @@ from django.db.models.functions import TruncDay, TruncMonth
 from django.db.models import Count
 import catalog.models
 
-# print("\n\n----------- IN STATS VIEW TEST -----------------")
-#
-# # Get all sales (delivered orders) from database
-# orders = (catalog.models.Order.objects
-#           .filter(status="DE")
-#           .annotate(date=TruncDay("time"))
-#           .values("date")
-#           .annotate(dcount=Count("date"))
-#           )
-#
-# for order in orders:
-#     print(order)
+print("\n\n----------- IN STATS VIEW TEST -----------------")
 
-#
-# print("------------------------------------------------\n\n")
+# Get all sales (delivered orders) from database
+item_id = 8
+orders_id = (catalog.models.OrderItem.objects
+             .filter(item_id=item_id)
+             .values_list("order_id")
+             )
+
+orders = (catalog.models.Order.objects
+          .filter(id__in=orders_id, status="DE")
+          .annotate(date=TruncDay("time"))
+          .values("date")
+          .annotate(sale_count=Count("date"))
+          )
+
+for order in orders:
+    print(order)
+
+print("------------------------------------------------\n\n")
 
 
 def get_total_sales(request):
-
     # В orders хранится пары <дата с точностью до дня, сколько заказов выполнено в этот день>
     orders = (catalog.models.Order.objects
               .filter(status="DE")
@@ -40,7 +44,6 @@ def get_total_sales(request):
         labels.append(date)
         data.append(sale_count)
 
-
     return render(request, "stats/charts.html", context={
         "labels": labels,
         "data": data
@@ -48,6 +51,31 @@ def get_total_sales(request):
 
 
 def get_item_sales(request, item_id):
-    # TODO
-    # Return sales of current item
-    return HttpResponse()
+    # Orders with specified item
+    orders_id = (catalog.models.OrderItem.objects
+                 .filter(item_id=item_id)
+                 .values_list("order_id")
+                 )
+
+    orders = (catalog.models.Order.objects
+              .filter(id__in=orders_id, status="DE")
+              .annotate(date=TruncDay("time"))
+              .values("date")
+              .annotate(sale_count=Count("date"))
+              )
+
+    labels = []
+    data = []
+    item_name = str(catalog.models.Item.objects.get(id=item_id))
+
+    for order in orders:
+        date = order["date"].strftime("%D")
+        sale_count = order["sale_count"]
+        labels.append(date)
+        data.append(sale_count)
+
+    return render(request, "stats/charts.html", context={
+        "labels": labels,
+        "data": data,
+        "item_name": item_name
+    })
