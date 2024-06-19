9d6d1d5f854b937e109a2506a82ef2f3d7d5fcbf
a1trope <bigtinyturtle@gmail.com>
2024-06-19 12:41:52 +0400

    update model

--- riotrecords/cart/models.py
+++ riotrecords/cart/models.py
@@ -7,3 +7,6 @@ class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"CartItem(id={self.id}, user={self.user}, item={self.item})"
