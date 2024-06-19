cae371f0fad2789b999795673e208f957471fb9c
a1trope <bigtinyturtle@gmail.com>
2024-06-19 13:55:13 +0400

    adding form for ordering

--- riotrecords/cart/urls.py
+++ riotrecords/cart/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
     path('delete/<int:cartitem_id>/', views.delete_from_cart, name='delete_from_cart'),
+    path('order/', views.order, name='order'),
 ]
