393e32f9a08a970cfff34f1322513e80f47a3ea1
a1trope <bigtinyturtle@gmail.com>
2024-06-19 13:02:42 +0400

    fix

--- riotrecords/cart/views.py
+++ riotrecords/cart/views.py
@@ -3,14 +3,13 @@ from .models import CartItem
 from catalog.models import Item
 from django.contrib.auth.decorators import login_required
 
-
-@login_required
+@login_required(login_url="accounts:login")
 def cart_detail(request):
     cart_items = CartItem.objects.filter(user=request.user)
     return render(request, 'cart/cart_detail.html', {'cart_items': cart_items})
 
 
-@login_required
+@login_required(login_url="accounts:login")
 def add_to_cart(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     user = request.user
@@ -21,7 +20,7 @@ def add_to_cart(request, item_id):
     return redirect("catalog:index")
 
 
-@login_required
+@login_required(login_url="accounts:login")
 def delete_from_cart(request, cartitem_id):
     user = request.user
     cart_item = get_object_or_404(CartItem, id=cartitem_id, user=user)
